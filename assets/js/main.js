cff095bff853d17411b42980def187e3c875c6ea
Yusuf C <letsudavid@gmail.com>
2022-03-20 07:55:15 -0700

    added a slider and udated css

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,19 @@ const btn = document.querySelector("#btnGenerate");
 const toggleUpper = document.querySelector("#upperCase"); 
 const toggleLower = document.querySelector("#lowerCase"); 
 const toggleSymbol = document.querySelector("#symbolToggle");
-const charLength = document.querySelector("#charLength");
-
 
 const numbers = [1,2,3,4,5,6,7,8,9,0];
-const symbols = ["$","#","!","%","@"];
+const symbols = ["`","!", "#","$", "%", "&", "(", ")","-","_","{","[", "]","}", "?", "<", ">", "*", "^"];
 const upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f','g', 'h', 'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r','s', 't', 'u', 'v', 'w', 'x','y', 'z'];
+
+const slider = document.getElementById("sliderMain");
+const output = document.getElementById("sliderOutValue");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+    output.innerHTML = this.value;
+  }
 
 function userSelection () { 
     let options = [...numbers]
@@ -25,6 +31,7 @@ function userSelection () {
     if (toggleSymbol.checked){
         options.push(...symbols);
     } 
+
     return options
 }
 
@@ -35,8 +42,8 @@ function generateRandomIdx (arrayLength) {
 function generatePass () {
     let results = [];
     let optionsArray = userSelection()
-    const userLength = charLength.checked ? 20: 10; 
-    for (let i = 0; i < userLength; i++){
+    const userLength = slider.checked;
+    for (let i = 0; i < slider.value; i++){
         const idx = generateRandomIdx(optionsArray.length);
         results.push(optionsArray[idx])
     }   
